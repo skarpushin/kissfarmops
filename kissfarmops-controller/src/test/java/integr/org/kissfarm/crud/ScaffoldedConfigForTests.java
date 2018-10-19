8117bdc511241e13199e475daff53e25e2867bc1
Sergey Karpushin <sergey.v.karpushin@gmail.com>
2018-10-20 00:37:58 +0300

    Added alternative (de)serialization for objects to json when working with DB

--- kissfarmops-controller/src/test/java/integr/org/kissfarm/crud/ScaffoldedConfigForTests.java
+++ kissfarmops-controller/src/test/java/integr/org/kissfarm/crud/ScaffoldedConfigForTests.java
@@ -1,12 +1,15 @@
 package integr.org.kissfarm.crud;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.kissfarm.controller.services.app_instance.api.AppInstanceService;
 import org.kissfarm.controller.services.nodes.api.NodeService;
 import org.kissfarm.shared.config.dto.StatusSchemaFromStringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
+import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.summerb.approaches.jdbccrud.scaffold.api.EasyCrudScaffold;
 
 @Configuration
@@ -24,4 +27,20 @@ public class ScaffoldedConfigForTests {
 	public AppInstanceService appInstanceService() {
 		return easyCrudScaffold.fromService(AppInstanceService.class, AppInstanceService.TERM, "app_instance");
 	}
+
+	@Bean
+	public ConversionServiceFactoryBean conversionServiceFactoryBean() {
+		ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
+		bean.setConverters(new HashSet<>(Arrays.asList(new StatusSchemaFromStringConverter())));
+		return bean;
+	}
+
+//	@Bean
+//	public ConversionService getConversionService() {
+//		ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
+//		bean.setConverters(new HashSet<>(Arrays.asList(new StatusSchemaFromStringConverter())));
+//		bean.afterPropertiesSet();
+//		ConversionService object = bean.getObject();
+//		return object;
+//	}
 }
